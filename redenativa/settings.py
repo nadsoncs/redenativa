801ac086d6d57ae29a8da319951fefb7ba053b3f
Nadson Cerqueira <nadsoncerqueira@gmail.com>
2020-09-04 16:08:57 -0300

    adicionando url de teste

--- redenativa/settings.py
+++ redenativa/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = ['api-redenativa.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['api-redenativa.herokuapp.com', 'api-redenativa-dev.herokuapp.com','localhost']
 #ALLOWED_HOSTS = ['localhost']
 
 # Application definition
