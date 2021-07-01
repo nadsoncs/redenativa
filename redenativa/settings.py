2614a434e855d2fb8ae83cd9c5a45bb4296a549e
Nadson Cerqueira <nadsoncerqueira@gmail.com>
2021-07-01 15:58:20 -0300

    Update settings.py

--- redenativa/settings.py
+++ redenativa/settings.py
@@ -28,7 +28,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'reo0h$bn#68882r&3l7q(bv08)uc!7peca^c$if3m#_^4!gj)_'
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
